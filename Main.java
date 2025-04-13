b80c4fd20349ebfef0f85d55262f5598aa2331ae
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-13 23:05:10 +0530

    scanner input

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Menu for calculator operations
+        System.out.println("Welcome to Java Calculator");
+        System.out.println("Choose Operation:");
+        System.out.println("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Square\n6. Cube\n7. Square Root");
+
+        int choice = sc.nextInt();
+
 }
