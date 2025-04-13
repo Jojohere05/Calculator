3622a8634f603884edaf211a8cd96f59e1b6e488
Jojohere05 <2005jhotikaraja@gmail.com>
2025-04-13 19:18:15 +0530

    division by 0 exception

--- Division.java
+++ Division.java
@@ -1,3 +1,7 @@
 public class Division {
-    
+    public static double divide(double a, double b) {
+        if (b==0){
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+       
 }
